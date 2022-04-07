4f47ff83093d53a4145e602056b92f3ef5fe4d54
SteffanoAsian <steffano.asianbudisetyo@gmail.com>
2022-04-07 19:55:46 +0700

    First Push

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.layout.activity_main);
+        viewPager.setAdapter(viewPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+}
